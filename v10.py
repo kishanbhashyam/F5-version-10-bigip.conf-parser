3d924ba640abab22a43f0eee9503c4ca925d7cce
Kishan Bhashyam <kishanbhashyam@hotmail.com>
2016-08-24 10:11:54 +1200

    Update v10.py

--- v10.py
+++ v10.py
@@ -9,67 +9,117 @@
 
 import re
 import sqlite3
+import sys
 
-#Creating SQL TABLE
-sqlite_file = '/.../.../.../F5ver10.sqlite3'
-table_name1 = 'table1'
-id_column = 'Virtual_Name'
-new_column1 = 'Virtual_IP'
-new_column2 = 'Pool_Name'
-new_column3 = 'Pool_IP'
-new_field = 'my_1st_column'
+#SOME VARIABLES DECLARED
 seenrule = 'false'
 strings = ("pool p", "pool P")
 
-conn = sqlite3.connect(sqlite_file)
+#Table 1 Variables
+F5_db = '/home/t816874/F5conf/F5.sqlite3'
+table_name1 = 'PoolTab'
+id_column1 = 'Pool_Name'
+id_column2 = 'Pool_IP'
+new_field = 'my_1st_column'
+
+conn = sqlite3.connect(F5_db)
+print "Opened database successfully";
 cur = conn.cursor()
 
-cur.execute('CREATE TABLE IF NOT EXISTS {tn} ({nf}, {nf1}, {nf2}, {nf3})'\
-        .format(tn=table_name1, nf=id_column, nf1=new_column1, nf2=new_column2, nf3=new_column3))
-
+#Create Table1 if does not exist already, else delete contents.
+cur.execute('CREATE TABLE IF NOT EXISTS {tn} ({nf}, {nf1})'\
+        .format(tn=table_name1, nf=id_column1, nf1=id_column2))
 cur.execute('DELETE FROM {tn}'\
         .format(tn=table_name1))
 
-File = open("/.../.../F5conf/unzip/config/bigip.conf", "r")
+#Call BigIP Conf File
+File = open("/home/t816874/F5conf/unzip/config/bigip.conf", "r")
+
+#Read File from desired string
 for line in File:
         if line.startswith (strings):
-
+                re.match("(^pool p)", line, re.IGNORECASE)
+                a = line.lstrip("pool p")
+                ONE = a[:-2]
+#               print
+#               print "Pool Name:", ONE
 
 #ALL Pool Names
                 for line in File:
                         if re.match("(^pool p)", line, re.IGNORECASE):
                                 a = line.lstrip("pool p")
                                 ONE = a[:-2]
-#                                print
-#                                print "Pool Name:", ONE
-                                cur.execute("INSERT INTO table1 (`Pool_Name`) VALUES (?)", (ONE, ));
+#                               print
+#                               print "Pool Name:", ONE
+#                               cur.execute("INSERT INTO PoolTab (`Pool_Name`) VALUES (?)", (ONE, ));
+#      
 #POOL MEMBER IP Regex
                         if re.findall(r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})', line):
                                 if seenrule == 'false':
                                         x = line.lstrip("members ")
                                         sep = ':'
                                         y = x.split(sep, 1)[0]
-#                                        print y
-                                        cur.execute("INSERT INTO table1 (`Pool_IP`) VALUES (?)",(y, ));
+#                                       print y
+                                        cur.execute("INSERT INTO PoolTab (`Pool_Name`, `Pool_IP`) VALUES (?,?)",(ONE, y));
+#                                       cur.execute("INSERT INTO PoolTab (`Pool_IP`) VALUES (?)",(y, ));
 
                         if re.match("(^rule )", line):
                                 seenrule = 'true'
+
+
+conn.execute("VACUUM")
+conn.commit()
+conn.close()
+print "Pool Table created and stored successfully";
+#######################-----------DATABASE ONE [pool_db] DONE-----------#######################
+
+#Table 2 Variables
+
+virtual_db = '/home/t816874/F5conf/F5.sqlite3'
+table_name2 = 'VirTab'
+id_column1 = 'Virtual_Name'
+id_column2 = 'Virtual_IP'
+new_field = 'my_1st_column'
+
+conn = sqlite3.connect(virtual_db)
+#print "Opened database successfully";
+cur = conn.cursor()
+
+#Create Table2 if does not exist already, else delete contents.
+cur.execute('CREATE TABLE IF NOT EXISTS {tn} ({nf}, {nf1})'\
+        .format(tn=table_name2, nf=id_column1, nf1=id_column2))
+cur.execute('DELETE FROM {tn}'\
+        .format(tn=table_name2))
+
+#Call BigIP Conf File
+File = open("/home/t816874/F5conf/unzip/config/bigip.conf", "r")
+
+#Read File from desired string
+for line in File:
+        if line.startswith (strings):
+
 #ALL Virtual Names
+                for line in File:
                         if re.match("(^virtual v)", line):
                                 b = line.lstrip("virtual v")
                                 TWO =  b[:-2]
-#                                print "Virtual Name: ", TWO
-
+#                               print "Virtual Name: ", TWO
+#                               cur.execute("INSERT INTO VirTab (`Virtual_Name`) VALUES (?)",(TWO, ));
 
 #ALL Virtual IPs
                         if re.match("(\s{,4}destination\s)", line):
                                 c = line.lstrip("destination ")
                                 THREE = c.split(':', 1 )[0]
-#                                print "Virtual IP  : ", THREE
-#                                print
-                                cur.execute("INSERT INTO table1 (`Virtual_Name`, `Virtual_IP`) VALUES (?,?)",(TWO, THREE));
+#                               print "Virtual IP  : ", THREE
+#                               print
+#                               cur.execute("INSERT INTO VirTab (`Virtual_IP`) VALUES (?)",(THREE, ));
+                                cur.execute("INSERT INTO VirTab (`Virtual_Name`, `Virtual_IP`) VALUES (?,?)",(TWO, THREE));
 
 conn.execute("VACUUM")
 conn.commit()
 conn.close()
-print "Result Printed into database /.../.../F5conf/F5ver10.sqlite3"
+print "Virtual Table created and stored successfully";
+#######################-----------DATABASE ONE [virtual_db] DONE-----------#######################
+######END
+########OF
+###SCRIPT
