c7adc7701b3d2adfe355b41878ab112288c9a80e
Kishan Bhashyam <kishanbhashyam@hotmail.com>
2016-08-24 10:22:00 +1200

    Update v10.py

--- v10.py
+++ v10.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # Author: Kishan Bhashyam
-# Last modified date: 17th August 2016
+# Last modified date: 24th August 2016
 # Version: V1.00
 # Purpose: This script will parse the TCNZ F5 backup v10 config and extract useful data out of it.
 #
@@ -16,7 +16,7 @@ seenrule = 'false'
 strings = ("pool p", "pool P")
 
 #Table 1 Variables
-F5_db = '/..../....../F5conf/F5.sqlite3'
+F5_db = '/home/t816874/F5conf/F5.sqlite3'
 table_name1 = 'PoolTab'
 id_column1 = 'Pool_Name'
 id_column2 = 'Pool_IP'
@@ -33,38 +33,38 @@ cur.execute('DELETE FROM {tn}'\
         .format(tn=table_name1))
 
 #Call BigIP Conf File
-File = open("/..../....../F5conf/unzip/config/bigip.conf", "r")
+File = open("/home/t816874/F5conf/unzip/config/bigip.conf", "r")
 
 #Read File from desired string
 for line in File:
         if line.startswith (strings):
-                re.match("(^pool p)", line, re.IGNORECASE)
+		re.match("(^pool p)", line, re.IGNORECASE)
                 a = line.lstrip("pool p")
                 ONE = a[:-2]
 #               print
 #               print "Pool Name:", ONE
 
 #ALL Pool Names
-                for line in File:
-                        if re.match("(^pool p)", line, re.IGNORECASE):
-                                a = line.lstrip("pool p")
-                                ONE = a[:-2]
-#                               print
-#                               print "Pool Name:", ONE
-#                               cur.execute("INSERT INTO PoolTab (`Pool_Name`) VALUES (?)", (ONE, ));
-#      
+               	for line in File:
+                       	if re.match("(^pool p)", line, re.IGNORECASE):
+                               	a = line.lstrip("pool p")
+                               	ONE = a[:-2]
+#				print
+#                              	print "Pool Name:", ONE
+#                             	cur.execute("INSERT INTO PoolTab (`Pool_Name`) VALUES (?)", (ONE, ));
+#				print "im here"
 #POOL MEMBER IP Regex
-                        if re.findall(r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})', line):
-                                if seenrule == 'false':
-                                        x = line.lstrip("members ")
-                                        sep = ':'
-                                        y = x.split(sep, 1)[0]
-#                                       print y
-                                        cur.execute("INSERT INTO PoolTab (`Pool_Name`, `Pool_IP`) VALUES (?,?)",(ONE, y));
-#                                       cur.execute("INSERT INTO PoolTab (`Pool_IP`) VALUES (?)",(y, ));
+                	if re.findall(r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})', line):
+       	                	if seenrule == 'false':
+               	                	x = line.lstrip("members ")
+                       	        	sep = ':'
+                               		y = x.split(sep, 1)[0]
+#                              		print y
+                              		cur.execute("INSERT INTO PoolTab (`Pool_Name`, `Pool_IP`) VALUES (?,?)",(ONE, y));
+#					cur.execute("INSERT INTO PoolTab (`Pool_IP`) VALUES (?)",(y, ));
 
-                        if re.match("(^rule )", line):
-                                seenrule = 'true'
+                       	if re.match("(^rule )", line):
+                               	seenrule = 'true'
 
 
 conn.execute("VACUUM")
@@ -75,7 +75,7 @@ print "Pool Table created and stored successfully";
 
 #Table 2 Variables
 
-virtual_db = '/..../....../F5conf/F5.sqlite3'
+virtual_db = '/home/t816874/F5conf/F5.sqlite3'
 table_name2 = 'VirTab'
 id_column1 = 'Virtual_Name'
 id_column2 = 'Virtual_IP'
@@ -92,15 +92,20 @@ cur.execute('DELETE FROM {tn}'\
         .format(tn=table_name2))
 
 #Call BigIP Conf File
-File = open("/..../....../F5conf/unzip/config/bigip.conf", "r")
+File = open("/home/t816874/F5conf/unzip/config/bigip.conf", "r")
 
 #Read File from desired string
 for line in File:
         if line.startswith (strings):
+		re.match("(^virtual v)", line)
+                b = line.lstrip("virtual v")
+                TWO =  b[:-2]
+#               print "Virtual Name: ", TWO
+#               cur.execute("INSERT INTO VirTab (`Virtual_Name`) VALUES (?)",(TWO, ));
 
 #ALL Virtual Names
-                for line in File:
-                        if re.match("(^virtual v)", line):
+		for line in File:
+			if re.match("(^virtual v)", line):
                                 b = line.lstrip("virtual v")
                                 TWO =  b[:-2]
 #                               print "Virtual Name: ", TWO
